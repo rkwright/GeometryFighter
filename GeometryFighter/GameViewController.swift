6a1ca7344fb74a4dfa8b1172bfe93294246bfed2
rkwright <rkwright@geofx.com>
2021-01-01 10:32:32 -0800

    Added support for random colors on the objects

--- GeometryFighter/GameViewController.swift
+++ GeometryFighter/GameViewController.swift
@@ -57,7 +57,7 @@ class GameViewController: UIViewController {
       // 2
       cameraNode.camera = SCNCamera()
       // 3
-      cameraNode.position = SCNVector3(x: 0, y: 0, z: 10)
+      cameraNode.position = SCNVector3(x: 0, y: 5, z: 10)
       // 4
       scnScene.rootNode.addChildNode(cameraNode)
     }
@@ -71,39 +71,57 @@ class GameViewController: UIViewController {
         case ShapeType.sphere:
             let sphere = SCNSphere(radius: 1.0)
             shapeNode = SCNNode(geometry: sphere)
+            sphere.materials.first?.diffuse.contents = UIColor.random()
 
         case ShapeType.pyramid:
             let pyra = SCNPyramid(width: 1.0, height:1.0, length:1.0)
             shapeNode = SCNNode(geometry: pyra)
-            
+            pyra.materials.first?.diffuse.contents = UIColor.random()
+
         case ShapeType.torus:
             let tor = SCNTorus(ringRadius: 1.0, pipeRadius:1.0)
             shapeNode = SCNNode(geometry: tor)
-            
+            tor.materials.first?.diffuse.contents = UIColor.random()
+
         case ShapeType.capsule:
             let caps = SCNCapsule(capRadius: 1.0, height:1.0)
             shapeNode = SCNNode(geometry: caps)
-            
+            caps.materials.first?.diffuse.contents = UIColor.random()
+
         case ShapeType.cylinder:
-            let tor = SCNCylinder(radius: 1.0, height:1.0)
-            shapeNode = SCNNode(geometry: tor)
-            
+            let cyl = SCNCylinder(radius: 1.0, height:1.0)
+            shapeNode = SCNNode(geometry: cyl)
+            cyl.materials.first?.diffuse.contents = UIColor.random()
+
         case ShapeType.cone:
             let con = SCNCone(topRadius: 0.0, bottomRadius:1.0, height:1.0)
             shapeNode = SCNNode(geometry: con)
-            
+            con.materials.first?.diffuse.contents = UIColor.random()
+
         case ShapeType.tube:
             let tub = SCNTube(innerRadius: 0.5, outerRadius:1.0, height:1.0)
             shapeNode = SCNNode(geometry: tub)
+            tub.materials.first?.diffuse.contents = UIColor.random()
 
         default:           // default is a ShapeType.box
             let box = SCNBox(width: 1.0, height: 1.0, length: 1.0, chamferRadius: 0.0)
             shapeNode = SCNNode(geometry: box)
+            box.materials.first?.diffuse.contents = UIColor.random()
      }
             
       shapeNode.physicsBody = SCNPhysicsBody(type: .dynamic, shape: nil)
 
-      scnScene.rootNode.addChildNode(shapeNode)
+        
+        let randomX = Float.random(min: -2, max: 2)
+        let randomY = Float.random(min: 10, max: 18)
+        
+        let force = SCNVector3(x: randomX, y: randomY , z: 0)
+        
+        let position = SCNVector3(x: 0.05, y: 0.05, z: 0.05)
+        
+        shapeNode.physicsBody?.applyForce(force, at: position, asImpulse: true)
+
+        scnScene.rootNode.addChildNode(shapeNode)
     }
 
 }
