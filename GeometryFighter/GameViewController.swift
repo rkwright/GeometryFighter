24fd92c0f569d2903aac5999f0cf457fe5d72e7a
rkwright <rkwright@geofx.com>
2021-01-01 08:49:13 -0800

    Initial steps to implementing part 3 of the tutorial

--- GeometryFighter/GameViewController.swift
+++ GeometryFighter/GameViewController.swift
@@ -11,12 +11,16 @@ import SceneKit
 class GameViewController: UIViewController {
     var scnView: SCNView!
     var scnScene: SCNScene!
+    var cameraNode: SCNNode!
 
   override func viewDidLoad() {
     super.viewDidLoad()
     
     setupView()
     setupScene()
+    setupCamera()
+    
+    spawnShape()
   }
 
   override var shouldAutorotate: Bool {
@@ -29,6 +33,14 @@ class GameViewController: UIViewController {
     
     func setupView() {
       scnView = self.view as! SCNView
+        
+        // 1
+        scnView.showsStatistics = true
+        // 2
+        scnView.allowsCameraControl = true
+        // 3
+        scnView.autoenablesDefaultLighting = true
+
     }
 
     func setupScene() {
@@ -37,6 +49,33 @@ class GameViewController: UIViewController {
         
       scnScene.background.contents = "GeometryFighter.scnassets/Textures/Background_Diffuse.png"
 
+    }
+
+    func setupCamera() {
+      // 1
+      cameraNode = SCNNode()
+      // 2
+      cameraNode.camera = SCNCamera()
+      // 3
+      cameraNode.position = SCNVector3(x: 0, y: 0, z: 10)
+      // 4
+      scnScene.rootNode.addChildNode(cameraNode)
+    }
+
+    func spawnShape() {
+      // 1
+      var geometry:SCNGeometry
+      // 2
+      switch ShapeType.random() {
+      default:
+        // 3
+        geometry = SCNBox(width: 1.0, height: 1.0, length: 1.0,
+          chamferRadius: 0.0)
+      }
+      // 4
+      let geometryNode = SCNNode(geometry: geometry)
+      // 5
+      scnScene.rootNode.addChildNode(geometryNode)
     }
 
 }
